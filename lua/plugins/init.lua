7013363c7da87c16086f03e0b582879d1b4c9157
qianxi0410 <894871277@qq.com>
2022-04-06 21:53:02 +0800

    feat: motion

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -125,6 +125,16 @@ return packer.startup(function(use)
   -- show color in hex
   use "norcalli/nvim-colorizer.lua"
 
+  -- motion
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v1', -- optional but strongly recommended
+    config = function()
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
