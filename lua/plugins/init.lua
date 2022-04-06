3fbdaf9b241cd928ece2bf7aa6f4cc949b9ed611
qianxi0410 <894871277@qq.com>
2022-04-06 21:39:07 +0800

    feat: courseline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,6 +65,8 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
 
+  use "yamatsum/nvim-cursorline"
+
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
