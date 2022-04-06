ddf2e3f2b95f99e7066c01ea10065ee39d654df1
qianxi0410 <894871277@qq.com>
2022-04-06 16:29:22 +0800

    feat: colorizer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
   -- markdown preview
   use "iamcco/markdown-preview.nvim"
 
+  -- show color in hex
+  use "norcalli/nvim-colorizer.lua"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
