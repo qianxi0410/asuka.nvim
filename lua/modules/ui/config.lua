6cfdbfc1bbc65e51a7e8c246e3c9f727d83784ac
qianxi0410 <894871277@qq.com>
2022-09-03 23:54:11 +0800

    chore: only show error and warn level msg

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -134,7 +134,7 @@ function config.notify()
 		---@usage minimum width for notification windows
 		minimum_width = 50,
 		---@usage notifications with level lower than this would be ignored. [ERROR > WARN > INFO > DEBUG > TRACE]
-		level = "TRACE",
+		level = "WARN",
 		---@usage Icons for the different levels
 		icons = {
 			ERROR = "",
