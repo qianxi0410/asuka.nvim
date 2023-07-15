b11b67e18730bd9ec8a7d7150e490aabc1110db0
qianxi0410 <894871277@qq.com>
2023-07-15 12:34:39 +0800

    feat: foramt justify

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -14,37 +14,6 @@ return function()
 		})
 	end
 
-	-- Please set additional flags for the supported servers here
-	-- Don't specify any config here if you are using the default one.
-	local sources = {
-		btns.formatting.clang_format.with({
-			filetypes = { "c", "cpp" },
-			extra_args = require("completion.formatters.clang_format"),
-		}),
-		btns.formatting.prettier.with({
-			filetypes = {
-				"vue",
-				"typescript",
-				"javascript",
-				"typescriptreact",
-				"javascriptreact",
-				"yaml",
-				"html",
-				"css",
-				"scss",
-				"sh",
-				"markdown",
-			},
-		}),
-	}
-	null_ls.setup({
-		border = "rounded",
-		debug = false,
-		log_level = "warn",
-		update_in_insert = false,
-		sources = sources,
-	})
-
 	mason_null_ls.setup({
 		ensure_installed = require("core.settings").null_ls_deps,
 		automatic_installation = false,
