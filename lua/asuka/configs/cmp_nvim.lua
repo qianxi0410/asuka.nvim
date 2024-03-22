dacf6882d935a67d2b90e66c9ec021fbcd0524d9
qianxi0410 <894871277@qq.com>
2024-03-22 14:09:15 +0800

    增加代码检查，修改了几个不合规范的地方

--- lua/asuka/configs/cmp_nvim.lua
+++ lua/asuka/configs/cmp_nvim.lua
@@ -9,12 +9,6 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 -- 更改补全面板的背景色
 vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#1d2021" })
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
 cmp.setup {
   window = {
     completion = {
